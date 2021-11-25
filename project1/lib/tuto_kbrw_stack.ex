89d63ba19be043f6a8cfad7654d410e397c5f381
kevin bedu <kevin.bedu@kbrwadventure.com>
2021-11-25 10:07:55 +0100

    Path update

--- project1/lib/tuto_kbrw_stack.ex
+++ project1/lib/tuto_kbrw_stack.ex
@@ -6,10 +6,10 @@ defmodule Tuto_kbrw_stack do
     sup = {_ok,pid} = Serv_supervisor.start_link([])
     [{_, reg, _, _}] = Supervisor.which_children(pid)
 
-    path = Path.expand("~/Documents/FORMATIONS/Formation2/PRO/project1/resources/chap1/orders_dump/orders_chunk0.json")
+    path = Path.join(File.cwd!,"resources/chap1/orders_dump/orders_chunk0.json")
     JsonLoader.load_to_database(reg,path)
-    {:ok, orders} = Database.search(reg,[{"id", "nat_order000147796"},{"id", "nat_order000147741"},{"id", "42"}])
-    IO.inspect(orders)
+    {:ok, _orders} = Database.search(reg,[{"id", "nat_order000147796"},{"id", "nat_order000147741"},{"id", "42"}])
+    #IO.inspect(orders)
     sup
   end
 

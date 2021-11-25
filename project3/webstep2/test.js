8386738455c944302817b68ebc09ff7fd9948f96
kevin bedu <kevin.bedu@kbrwadventure.com>
2021-11-25 10:46:35 +0100

    deps again

---
+++ project3/webstep2/test.js
@@ -0,0 +1,13 @@
+import parser from "@babel/parser";
+
+const code = `function square(n) {
+  return n * n;
+}`;
+
+parser.parse(code);import parser from "@babel/parser";
+
+const code = `function square(n) {
+  return n * n;
+}`;
+
+parser.parse(code);

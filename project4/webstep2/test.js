1e5c49848644333ceab89128eda2600793552b48
kevin bedu <kevin.bedu@kbrwadventure.com>
2021-11-26 13:37:05 +0100

    project4

---
+++ project4/webstep2/test.js
@@ -0,0 +1,9 @@
+function toInvoke(){
+    var xxx = 18;
+    <Declaration var="test" value={42}/>
+    alert(test);
+    return "bipbip"
+
+}
+
+toInvoke();
---
+++ project4/webstep3/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+
+var createReactClass = require('create-react-class')
+
+var Page = createReactClass({
+render(){
+    return <JSXZ in="template" sel=".container">
+    <Z sel=".item">Burgers</Z>,
+    <Z sel=".price">50</Z>
+    </JSXZ>
+}
+})
+
+ReactDOM.render(
+<Page/>,
+document.getElementById('root')
+)
